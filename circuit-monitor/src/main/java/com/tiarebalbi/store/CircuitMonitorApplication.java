6f6853b2813ddb53944f694c6dda32bfb4728075
Tiare Balbi Bonamini <me@tiarebalbi.com>
2015-10-13 23:52:36 -0300

    Adicionado projetos para config-server e circuit monitor utilizando o Hydrix

---
+++ circuit-monitor/src/main/java/com/tiarebalbi/store/CircuitMonitorApplication.java
@@ -0,0 +1,12 @@
+package com.tiarebalbi.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircuitMonitorApplication.class, args);
+    }
+}
